8b1dabfb505163d23eb6e2e8ac37fb54464058e5
Suryaansh_Rathinam <suryaansh@greedygame.com>
2023-01-31 20:34:18 +0530

    contact page done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,33 +1,49 @@
 import "./Contact.css";
 import React, { useState, useEffect } from "react";
-import { AiOutlineDoubleRight } from "react-icons/ai";
+import {
+  AiOutlineDoubleRight,
+  AiFillLinkedin,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
 import { TiTick } from "react-icons/ti";
-
+import robot from "../../assets/robot.gif";
+import tyRobot from "../../assets/ty-robot.png";
 export default function Contact() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
+  const updateCount = (c) => {
+    document.getElementById(`c${c}`).style.opacity = 0;
+    document.getElementById(`btn-id`).style.opacity = 0;
+
+    setTimeout(() => {
+      setCount(count + 1);
+      setTimeout(() => {
+        document.getElementById(`c${c + 1}`).style.opacity = 1;
+        document.getElementById(`btn-id`).style.opacity = 1;
+      }, 500);
+    }, 500);
+  };
   useEffect(() => {
-    
-    if(count==4){
-      setTimeout(()=>{
+    if (count == 4) {
+      setTimeout(() => {
         window.location.reload();
       }, 5000);
-
     }
-   
-  }, [count])
-  
+  }, [count]);
+
   return (
     <div id="contact" className="contact-container">
+      {count == 0 && <div className="contact-title">Contact Us</div>}
       {count == 0 && (
-        <div className="contact-h">
+        <div className="contact-h3" id={"c0"}>
           Team MOSS is looking forward to getting in touch with you
         </div>
       )}
       {count == 1 && (
-        <div className="contact-h">
+        <div className="contact-h" id={"c1"}>
           Please Tell Us Your Name
           <input
             placeholder=""
@@ -37,7 +53,7 @@ export default function Contact() {
         </div>
       )}
       {count == 2 && (
-        <div className="contact-h">
+        <div className="contact-h" id={"c2"}>
           Hi {name}, could you please provide your email address
           <input
             placeholder=""
@@ -47,7 +63,7 @@ export default function Contact() {
         </div>
       )}
       {count == 3 && (
-        <div className="contact-h">
+        <div className="contact-h" id={"c3"}>
           What would you like to talk to us regarding?
           <input
             placeholder=""
@@ -57,32 +73,57 @@ export default function Contact() {
         </div>
       )}
       {count == 4 && (
-        <div className="contact-h">
-          Thank you for reaching out to us, we will get back to you shortly.
-        </div>
+        <>
+          <img src={tyRobot} className="ty-robot" id={"c4"} />
+          <div className="contact-h2">
+            We have recieved your message, will get back to you shortly.
+          </div>
+        </>
       )}
       <div className="">
         {count == 0 && (
-          <button className="next-btn" onClick={() => setCount(count + 1)}>
+          <button
+            className="next-btn"
+            style={{ opacity: 1 }}
+            id="btn-id"
+            onClick={() => updateCount(count)}
+          >
             NEXT >
           </button>
         )}
         {count > 0 && count < 3 && (
-          <button className="next-btn-2" onClick={() => setCount(count + 1)}>
+          <button
+            className="next-btn-2"
+            id="btn-id"
+            onClick={() => updateCount(count)}
+          >
             <AiOutlineDoubleRight className="dr" />
           </button>
         )}
         {count == 3 && (
-          <button className="next-btn-2" onClick={() => setCount(count + 1)}>
+          <button
+            className="next-btn-2"
+            id="btn-id"
+            onClick={() => updateCount(count)}
+          >
             <TiTick className="dr" />
           </button>
         )}
+        {count != 4 && <img src={robot} className="robot-gif" />}
+        {/* {count ==4 &&} */}
       </div>
-      <>
-        {/* <video width="750" height="500" controls >
-      <source src={video} type="video/mp4"/>
-     </video> */}
-      </>
+      <div className="icon-container">
+      <a href="https://www.linkedin.com/company/manipal-open-source-society/" target={'_blank'}>
+        <AiFillLinkedin className="contact-icon" />
+        </a>
+      <a href="https://www.instagram.com/moss.manipal/" target={'_blank'}>
+
+        <AiFillInstagram className="contact-icon" />
+        </a>
+        <a href="mailto:mossmanipal@gmail.com" target="_top">
+        <MdEmail className="contact-icon" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Contact from '../../Components/Contact/Contact'
 import TopNav from '../../Components/Navbar/Navbar'
+import About from '../About/About'
 import './Home.css'
 export default function Home() {
   return (
     <div className=''>
         <TopNav/>
+        <About/>
         <div className='main-container'>
-        Hello world
         <Contact/>
         </div>
     </div>
